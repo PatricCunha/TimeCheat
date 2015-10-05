306a07217d919a28ca11f5489e044d418f78060a
Patric Cunha <patriccunha@oulook.com>
2015-10-05 13:30:01 +0100

    Fixed day presentation for previous and subsequent months

--- timeCheat.py
+++ timeCheat.py
@@ -12,7 +12,18 @@ def name_to_num(abbr):
 	return list(calendar.month_name).index(abbr)
 def num_to_name(num):
 	return calendar.month_name[num]
-
+def diff_month_date(arr, month, start):
+	lastday = calendar.monthrange(date.today().year,month)[1]
+	otherMonthWeeks = currentCalendar.monthdays2calendar(date.today().year, month)
+	if(start):
+		otherMonthWeekDays = otherMonthWeeks[-1]
+	else:
+		otherMonthWeekDays = otherMonthWeeks[0]
+	for day in otherMonthWeekDays:
+		print day
+		print arr
+		if(day[1] == arr[1]):
+			return day[0]
 # check if there is a filename given
 assert len(sys.argv) > 1, "Filename not included in arguments"
 # get the name from the first argument
@@ -25,19 +36,22 @@ sheets = wb.get_sheet_names()
 lastmonth = sheets[-1]
 # as a numeric value
 lastmonthnum = name_to_num(lastmonth)
-# get the next month as a num
-lastmonthnum += 1
+# get this month as a num
+thismonthnum = lastmonthnum + 1
+# get the next month as num
+nextmonthnum = thismonthnum + 1
 # make sure we reset on december
 if lastmonthnum > 11 :
-	lastmonthnum = 1
+	thismonthnum = 1
 # get the month from the number
-month = num_to_name(lastmonthnum)
+month = num_to_name(thismonthnum)
+print month
 # create a new worksheet
 ws = wb.create_sheet()
 # change the title to the month
 ws.title = month
 # print month to console
-calendar.prmonth(2015, 8)
+calendar.prmonth(2015, thismonthnum)
 # new calendar starting each week on monday
 currentCalendar = calendar.Calendar(0)
 # let's create headers
@@ -60,13 +74,21 @@ noWorkStyleFill = PatternFill(patternType='solid',fgColor='FFEB9C')
 noWorkStyleFont = Font(name='Calibri',size=11,bold=False,italic=False,vertAlign=None,underline='none',strike=False,color='9C6500')
 noWorkStyle = Style(fill=noWorkStyleFill, font=noWorkStyleFont)
 # get datetime iterator for each day in month
-monthdates = currentCalendar.itermonthdays2(date.today().year, lastmonthnum)
+monthdates = currentCalendar.itermonthdays2(date.today().year, thismonthnum)
 monthStr = '='
+trueDate = 0
 for day in monthdates:
 	countStr = str(lineCounter)
+	if(day[0] == 0):
+		if(lineCounter <= 9):
+			trueDate = diff_month_date(day, lastmonthnum, True)
+		else:
+			trueDate = diff_month_date(day, nextmonthnum, False)
+	else:
+		trueDate = day[0]
 	if(day[1] == 5 or day[1] == 6):
 		cell = "A" + countStr
-		ws[cell] = day[0]
+		ws[cell] = trueDate
 		ws[cell].style = noWorkStyle
 		cell = "B" + countStr
 		ws[cell].style = noWorkStyle
@@ -78,7 +100,7 @@ for day in monthdates:
 		ws[cell].style = noWorkStyle
 	else:
 		cell = "A" + countStr
-		ws[cell] = day[0]
+		ws[cell] = trueDate
 		cell = "B" + countStr
 		ws[cell] = time(9,0,0,0)
 		cell = "C" + countStr
